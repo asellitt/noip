8500c7cf028c54e9630f5c42db85b0f2098a325f
Anthony Sellitti <anthony.sellitti@envato.com>
2015-10-19 15:33:34 +1100

    you need to manage the host as well buddy

--- lib/noip/credentials.rb
+++ lib/noip/credentials.rb
@@ -1,10 +1,13 @@
 module Noip
   class Credentials
-    def initialize(username = nil, password = nil)
+
+    def initialize(username = nil, password = nil, host = nil)
       @username = username || ENV['NOIP_USERNAME']
       @password = password || ENV['NOIP_PASSWORD']
+      @host     = host || ENV['NOIP_HOST']
     end
 
-    attr_reader :username, :password
+    attr_reader :username, :password, :host
+
   end
 end
--- spec/noip/credentials_spec.rb
+++ spec/noip/credentials_spec.rb
@@ -5,14 +5,16 @@ describe Noip::Credentials do
     before do
       allow(ENV).to receive(:[]).with('NOIP_USERNAME').and_return('env_noip_username')
       allow(ENV).to receive(:[]).with('NOIP_PASSWORD').and_return('env_noip_password')
+      allow(ENV).to receive(:[]).with('NOIP_HOST').and_return('env_noip_host')
     end
 
     describe 'username' do
-      subject(:username) { Noip::Credentials.new(username, password).username }
+      subject(:username) { Noip::Credentials.new(username, password, host).username }
 
       context 'with values provided' do
         let(:username) { 'provided_username'}
         let(:password) { 'provided_password'}
+        let(:host)     { 'provided_host'}
 
         it { is_expected.to eq 'provided_username' }
       end
@@ -20,17 +22,19 @@ describe Noip::Credentials do
       context 'with no values provided' do
         let(:username) { nil }
         let(:password) { nil }
+        let(:host)     { nil }
 
         it { is_expected.to eq 'env_noip_username' }
       end
     end
 
     describe 'password' do
-      subject(:password) { Noip::Credentials.new(username, password).password }
+      subject(:password) { Noip::Credentials.new(username, password, host).password }
 
       context 'with values provided' do
         let(:username) { 'provided_username'}
         let(:password) { 'provided_password'}
+        let(:host)     { 'provided_host'}
 
         it { is_expected.to eq 'provided_password' }
       end
@@ -38,8 +42,29 @@ describe Noip::Credentials do
       context 'with no values provided' do
         let(:username) { nil }
         let(:password) { nil }
+        let(:host)     { nil }
 
         it { is_expected.to eq 'env_noip_password' }
+      end
+    end
+
+    describe 'host' do
+      subject(:host) { Noip::Credentials.new(username, password, host).host }
+
+      context 'with values provided' do
+        let(:username) { 'provided_username'}
+        let(:password) { 'provided_password'}
+        let(:host)     { 'provided_host'}
+
+        it { is_expected.to eq 'provided_host' }
+      end
+
+      context 'with no values provided' do
+        let(:username) { nil }
+        let(:password) { nil }
+        let(:host)     { nil }
+
+        it { is_expected.to eq 'env_noip_host' }
       end
     end
   end
